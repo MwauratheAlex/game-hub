bd16b49d25298e0c33ef016eedb0d70c3651db8c
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-05 15:26:01 +0300

    Show loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,21 +24,28 @@ export interface Game {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
       const contoller = new AbortController();
 
+      setLoading(true);
       apiClient
         .get<FetchGamesResponse>("/games", { signal: contoller.signal })
-        .then((res) => setGames(res.data.results))
+        .then((res) => {
+          setGames(res.data.results)
+          setLoading(false);
+        })
         .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message);
+          if (err instanceof CanceledError) return;
+          setError(err.message);
+          setLoading(false);
         });
 
     return () => contoller.abort();
     }, []);
 
-    return {games, error};
+    return {games, error, loading};
 }
 
 export default useGames;
