2c05b7712dc2e9c1f389fea56c2695302e6b6497
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-07 08:42:27 +0300

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,4 +1,6 @@
 const getCroppedImageUrl = (url: string) => {
+    if (!url) return "";
+    
     const target = "media/"
     const idx = url.indexOf(target) + target.length;
     return url.slice(0, idx) + "crop/600/400/" + url.slice(idx);
