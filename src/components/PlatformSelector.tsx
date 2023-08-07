ea06c3043c36dd70a2ea6d5ee11ad97b9005dfe6
Mwaura Mbugua <mwaurathealex@gmail.com>
2023-08-07 07:52:17 +0300

    Refactor: Extract a query object

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -5,7 +5,7 @@ import { Platform } from "../hooks/useData";
 
 interface Props {
   onSelectPlatform: (platform: Platform) => void;
-  selectedPlatform: Platform;
+  selectedPlatform: Platform | null;
 }
 
 const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
